f7ce978ee00df41ad2688e6b818f065be2025550
Karen Gomes <kngs@ic.ufal.br>
2019-01-30 21:12:22 -0300

    feat(Tests): teste com JUnit puro para saber se a lista foi ordenada

--- src/test/java/com/karenngomes/sistema/Tests.java
+++ src/test/java/com/karenngomes/sistema/Tests.java
@@ -3,7 +3,11 @@ package com.karenngomes.sistema;
 import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.text.IsEqualIgnoringCase.equalToIgnoringCase;;
+import static org.hamcrest.text.IsEqualIgnoringCase.equalToIgnoringCase;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+import java.util.*;
 
 public class Tests {
 	@Test
@@ -12,5 +16,26 @@ public class Tests {
         String b = "FOO";
         assertThat(a, equalToIgnoringCase(b));
     }
+	
+	@Test
+	public void trazlistaOrdenadaComTodosOsElementos() {
+		ArrayList<Integer> lista = new ArrayList<Integer>();
+		
+		lista.add(5);
+		lista.add(3);
+		lista.add(2);
+		lista.add(7);
+		
+		Collections.sort(lista);	
+		
+		assertEquals(4, lista.size());
+		assertEquals(new Integer(2), lista.get(0));
+		assertEquals(new Integer(3), lista.get(1));
+		assertEquals(new Integer(5), lista.get(2));
+		assertEquals(new Integer(7), lista.get(3));
+				
+		
+		
+	}
 	
 }
